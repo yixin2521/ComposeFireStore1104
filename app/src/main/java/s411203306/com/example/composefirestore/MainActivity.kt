2617b87e5bbe7596601240c032a2bab421358fec
yixin2521 <1emonayu2521@gmail.com>
2024-11-04 15:24:44 +0800

    v2_姓名輸入

--- app/src/main/java/s411203306/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/s411203306/com/example/composefirestore/MainActivity.kt
@@ -50,6 +50,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Birth(m: Modifier){
+    var userName by remember { mutableStateOf("陳妤欣")}
+
+    Column {
+        TextField(
+            value = userName,
+            onValueChange = { newText ->
+                userName = newText
+            },
+            modifier = m,
+            label = { Text("姓名") },
+            placeholder = { Text("請輸入您的姓名") }
+
+        )
+
+        Text("您輸入的姓名是：$userName")
+    }
 
 
 }
